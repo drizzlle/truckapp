d431d9093b737c505e478f5d158ebe6f875ccb8a
Umar Aliyu Chalawa <liuomar90@gmail.com>
2025-11-13 15:54:30 +0100

    Implement user authentication API with Django REST framework

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,15 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
-
+#Rest Framework Configuration
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+           'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api',
     'accounts',
     'pages' 
